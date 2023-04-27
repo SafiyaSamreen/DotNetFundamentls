1565569a90ac6ffa5df146e7b3ba4ed98f145e11
SafiyaSamreen <92724659+SafiyaSamreen@users.noreply.github.com>
2023-04-27 21:34:10 +0530

    Add files via upload

---
+++ DotnetCoreConsoleDemo/DotnetCoreConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DotNetStandardLibDemo;
+
+namespace DotnetCoreConsoleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = args[0];
+            DateNameConcatination d = new DateNameConcatination();
+
+            var result = d.GetGreetings(name);
+
+            Console.WriteLine(result);
+        }
+    }
+}
