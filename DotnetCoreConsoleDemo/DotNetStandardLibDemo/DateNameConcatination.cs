0aa56501b3d984a93a8080274a36e66e5d60a75b
SafiyaSamreen <92724659+SafiyaSamreen@users.noreply.github.com>
2023-04-27 21:38:10 +0530

    Add files via upload

---
+++ DotnetCoreConsoleDemo/DotNetStandardLibDemo/DateNameConcatination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DotNetStandardLibDemo
+{
+    public class DateNameConcatination
+    {
+        public string GetGreetings(string Username)
+        {
+            var time = DateTime.Now;
+
+            var result = time + " " + "Hello, " + Username;
+
+            return result;
+        }
+    }
+}
